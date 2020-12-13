54100ea4975a72461235bf5ec9858102ad3738a1
David Hazan <david_shazan95@hotmail.com>
2020-12-14 01:49:05 +0200

    ENH: Fixed behave issue with Examples without any value and path assert being matched with another test

--- features/steps/utils.py
+++ features/steps/utils.py
@@ -5,17 +5,17 @@ def validate_path_regex(path):
     return "Path Syntax in Dataset is not valid!"
 
 def validate_dataset_empty_field(datafield):
-    if datafield["vendor"] == "":
+    if datafield["vendor"] == '""':
         return "The following field is empty: Vendor"
-    elif datafield["url"] == "":
+    elif datafield["url"] == '""':
         return "The following field is empty: URL"
-    elif datafield["series"] == "":
+    elif datafield["series"] == '""':
         return "The following field is empty: Series"
-    elif datafield["category"] == "":
+    elif datafield["category"] == '""':
         return "The following field is empty: Category"
-    elif datafield["model"] == "":
+    elif datafield["model"] == '""':
         return "The following field is empty: Model"
-    elif datafield["path"] == "":
+    elif datafield["path"] == '""':
         return "The following field is empty: Path"
-    elif datafield["release"] == "":
+    elif datafield["release"] == '""':
         return "The following field is empty: Release"
